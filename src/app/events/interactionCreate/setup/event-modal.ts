0c2c63cb58fe24b2ae1f7316be5c68bdc7baf787
git-cameronbryce <developmentcameronbryce@gmail.com>
2026-07-08 18:02:48 -0600

    refactor: difer response

--- src/app/events/interactionCreate/setup/event-modal.ts
+++ src/app/events/interactionCreate/setup/event-modal.ts
@@ -1,7 +1,9 @@
 import { db } from "@/app";
+import { generate } from "@/app/events/interactionCreate/setup/generate";
 import { guildsTable } from "@/app/database/guilds";
 import { TokenRouter } from "@/app/routes/token";
 import type { EventHandler } from "commandkit";
+import { eq } from "drizzle-orm";
 
 // @ts-ignore
 const handler: EventHandler<"interactionCreate"> = async (interaction) => {
@@ -10,24 +12,24 @@ const handler: EventHandler<"interactionCreate"> = async (interaction) => {
   if (interaction.customId === "modal-token") {
     const token = interaction.fields.getTextInputValue("modal-token");
     const guild = interaction.guildId!;
+    await interaction.deferReply();
 
     // prettier-ignore
     const scopes = (await new TokenRouter(token).get().catch(() => null))
       ?.data.token.scopes;
 
     if (!scopes || !scopes.includes("service")) {
-      return interaction.reply({
+      return interaction.followUp({
         content: "Token does not have the required scope: service",
         ephemeral: true,
       });
     }
 
-    await db
-      .insert(guildsTable)
-      .values({ guild, token })
-      .onConflictDoUpdate({ target: guildsTable.guild, set: { token } });
+    await db.delete(guildsTable).where(eq(guildsTable.guild, guild));
+    await db.insert(guildsTable).values({ guild, token });
+    await generate(interaction);
 
-    interaction.reply({
+    await interaction.followUp({
       content: `Token setup button clicked: ${token}`,
       ephemeral: true,
     });
