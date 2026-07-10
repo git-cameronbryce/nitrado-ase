36a243adf44e153ae64370873d603212cf2bd7c5
git-cameronbryce <developmentcameronbryce@gmail.com>
2026-07-09 19:50:03 -0600

    feature: add discordsTable

--- src/app/database/guilds.ts
+++ src/app/database/guilds.ts
@@ -2,5 +2,21 @@ import { pgTable, text } from "drizzle-orm/pg-core";
 
 export const guildsTable = pgTable("guild", {
   guild: text().primaryKey().notNull(),
-  token: text().notNull(),
+  token: text().notNull().unique(),
+});
+
+export const discordsTable = pgTable("discord", {
+  guild: text()
+    .primaryKey()
+    .references(() => guildsTable.guild, { onDelete: "cascade" }),
+
+  statusChannel: text().notNull(),
+  statusMessage: text().notNull(),
+  playerAudits: text().notNull(),
+  serverAudits: text().notNull(),
+
+  onlineLogging: text().notNull(),
+  adminLogging: text().notNull(),
+  chatLogging: text().notNull(),
+  joinLogging: text().notNull(),
 });
